60ff09092fd652f436de2dc326236773f6a53d13
Joshua Crum <joshua.crum@gmail.com>
2019-05-26 23:20:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,9 @@
 require 'pry'
 
 class TicTacToe
+  attr_accessor :board
 
-   def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
-
-   WIN_COMBINATIONS = [
+  WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -17,110 +14,87 @@ class TicTacToe
     [6,4,2]
   ]
 
-   def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  def initialize()
+    @board = Array.new(9, " ")
   end
 
-   def move(location, character = "X")
-    @board[location.to_i - 1] = character
-  end
-
-   def position_taken?(position)
-    if @board[position] == "X" || @board[position] == "O"
-      true
-    else
-      false
-    end
-  end
-
-   def valid_move?(position)
-    position = position.to_i - 1
-    if position.between?(0,8) && !position_taken?(position)
-      true
-    else
-      false
-    end
-  end
-
-   def turn
-    puts "Please enter 1-9:"
-    input = gets.strip
-    if valid_move?(input)
-      move(input, current_player)
-    else
+  def play
+    while !over?
       turn
     end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+  def turn
+    display_board
+    puts "Please enter 1-9:"
+    input = gets.strip
+    if !valid_move?(input)
+      turn
+    end
+    move(input, current_player)
     display_board
   end
 
-   def turn_count
-    counter = 0
-    @board.each do |i|
-      if i == "X" || i == "O"
-        counter += 1
-      end
-    end
-    counter
+  def valid_move?(index)	
+    !position_taken?(index) && index.between?(0,8)	
   end
 
-   def current_player
+  def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
 
-
-
-   def won?
-
-     board_empty = @board.none? { |i| i == "X" || i = "O"}
-    if board_empty
-      false
-    else
-      WIN_COMBINATIONS.each do |combo|
-        if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-          return combo
-        end
-      end
-      return false
-  end
-end
-
-   def full?
-    @board.all? { |i| i =="X" || i == "O"}
+  def full?
+    @board.all?{|token| token == "X" || token == "O"}
   end
 
-   def draw?
-    !won? && full? ? true : false
+  def over?
+    won? || draw?
   end
 
-   def over?
-    won? || draw? || full? ? true : false
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
   end
 
-   def winner
+  def move(index, token)	
+    @board[index] = token	
+  end
+
+  def won?
     WIN_COMBINATIONS.detect do |combo|
-          if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
-            return "X"
-          elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-            return "O"
-          else
-            nil
-          end
+      position(combo[0]) == position(combo[1]) &&
+      position(combo[1]) == position(combo[2]) &&
+      position_taken?(combo[0])
     end
   end
 
-   def play
-    until over?
-      turn
-    end
+  def draw?
+    !won? && @board.all?{|token| token == "X" || token == "O"}
+  end
 
-     if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cats Game!"
+  def winner
+    if winning_combo = won?
+      @winner = position(winning_combo.first)
     end
+  end
+
+  def position(location)
+    @board[location.to_i]
+  end
+
+  def position_taken?(location)
+    !(position(location).nil? || position(location) == " ")
   end
 end
